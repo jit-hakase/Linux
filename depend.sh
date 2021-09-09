6dbd4ed1bfa086daf246ee9233e4d8ddb070d948
hakase <cb634780751@qq.com>
2021-09-09 15:38:02 +0800

    Create depend.sh

---
+++ depend.sh
@@ -0,0 +1,8 @@
+yum install --downloadonly --downloaddir=tool lrzsz
+yum install --downloadonly --downloaddir=tool bc
+yum install --downloadonly --downloaddir=tool vim
+
+yum install --downloadonly --downloaddir=net net-tools
+yum install --downloadonly --downloaddir=net nc
+yum install --downloadonly --downloaddir=net tcpdump
+
