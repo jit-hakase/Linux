0845a7956f3f83a54c74e428e99c9599825d9167
hakase <cb634780751@qq.com>
2021-09-22 20:20:42 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-major_version=1
-minor_version=0
+main_version="1.0"
 base_hash=$(git rev-list --abbrev-commit --max-parents=0 HEAD)
 patch_version=$(git rev-list --abbrev-commit HEAD...${base_hash}|wc -l)
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 current_hash=$(git rev-parse --short HEAD)
-echo ${major_version}.${minor_version}.${patch_version}-${current_branch}-${current_hash}
+echo "${main_version}.${patch_version}-${current_branch}-${current_hash}"
